b4261dfec5b8fd7770386bf9cdecafe0cbc3dbce
miguel flores ramirez <miguelf1671@gmail.com>
2023-10-18 16:41:56 -0400

    nothing

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -3,27 +3,23 @@ import Games from "./Games";
 import Description from "./Description";
 import Nav from "../nav";
 
-export default function Home({URL})
-{
-    //state for holding game info
-    const [games, setGames] = useState([])
+export default function Home({ URL }) {
+  //state for holding game info
+  const [games, setGames] = useState([]);
 
-    //initial fetch for getting info from db.json
-    useEffect(() => {
-        fetch(URL)
-        .then(res => res.json())
-        .then(data => setGames(data))
-    },[])
+  //initial fetch for getting info from db.json
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => setGames(data));
+  }, []);
 
-
-    
-    //show navbar at all times to go to different pages
-    //and passing down games to both Games
-    return (
-        <div>
-            <Nav />
-            <Games Games={games}/>
-        </div>
-        
-    )
+  //show navbar at all times to go to different pages
+  //and passing down games to both Games
+  return (
+    <div>
+      <Nav />
+      <Games Games={games} />
+    </div>
+  );
 }
